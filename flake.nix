96ce887432d8f29ab7206b9d15d3bf55c11ca2ef
Philipp Middendorf <philipp.middendorf@desy.de>
2025-04-01 11:42:47 +0200

    ⬆️ Upgrade to nixos-24.11

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
   description = "simplon-stub-hs";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-24.05";
+    nixpkgs.url = "nixpkgs/nixos-24.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
